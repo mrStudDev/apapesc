651288db5f3fe79c83031c3950db9854ff056880
mrStudDev <marcosscfloripa@gmail.com>
2025-06-10 16:11:40 -0300

    Instruções Serviços

--- app_documentos/templatetags/documento_extras.py
+++ app_documentos/templatetags/documento_extras.py
@@ -1,6 +1,7 @@
 # app_documentos/templatetags/documento_extras.py
 import os
 from django import template
+from app_documentos.models import Documento
 
 register = template.Library()
 
@@ -10,3 +11,12 @@ def extensao(arquivo_field):
         return ''
     nome_arquivo = str(arquivo_field.name)
     return os.path.splitext(nome_arquivo)[-1].replace('.', '').upper()
+
+
+@register.simple_tag
+def get_docs_padrao(*tipos):
+    return Documento.objects.filter(
+        repositorio_padrao=True,
+        tipo_doc__tipo__in=tipos
+    ).select_related('tipo_doc')
+
